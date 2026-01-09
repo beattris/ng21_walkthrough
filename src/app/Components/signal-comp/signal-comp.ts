266385eb3062355dc74a56144583123c22c76189
Beattris <beatriceebirim@gmail.com>
2026-01-09 05:20:05 +0100

    added bootstrap

---
+++ src/app/Components/signal-comp/signal-comp.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-signal-comp',
+  imports: [],
+  templateUrl: './signal-comp.html',
+  styleUrl: './signal-comp.css',
+})
+export class SignalComp {
+
+}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { DataBinding } from './Components/data-binding/data-binding';
+import { SignalComp } from './Components/signal-comp/signal-comp';
+import { Variable } from './Components/variable/variable';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'Databinding', component: DataBinding },
+  { path: 'Signal', component: SignalComp },
+  { path: 'Variables', component: Variable },
+];
