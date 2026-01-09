dd961709ff670b0c400e4bcabb5be1443e9432e9
Beattris <beatriceebirim@gmail.com>
2026-01-09 06:07:55 +0100

    added a notFound component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { DataBinding } from './Components/data-binding/data-binding';
 import { SignalComp } from './Components/signal-comp/signal-comp';
 import { Variable } from './Components/variable/variable';
+import { NotFoundComponent } from './Components/not-found-component/not-found-component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'databinding', pathMatch: 'full' },
   { path: 'databinding', component: DataBinding },
   { path: 'signal', component: SignalComp },
-  { path: 'variables', component: Variable },
+    { path: 'variables', component: Variable },
+  {path: '**', component: NotFoundComponent}
 ];
