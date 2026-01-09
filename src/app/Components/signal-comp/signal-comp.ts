828b6534eef11fbbeae4b67ceca575786c2d8b18
Beattris <beatriceebirim@gmail.com>
2026-01-09 06:47:56 +0100

    created and used signals

--- src/app/Components/signal-comp/signal-comp.ts
+++ src/app/Components/signal-comp/signal-comp.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Signal, signal } from '@angular/core';
 
 @Component({
   selector: 'app-signal-comp',
@@ -7,5 +7,31 @@ import { Component } from '@angular/core';
   styleUrl: './signal-comp.css',
 })
 export class SignalComp {
+  // courseName: string = 'NG 21';
 
+  // CREATING A SIGNAL
+  courseName = signal('NG 21');
+
+  // WE CAN STORE OTHER THINGS IN A SIGNAL (NOT JUST STRINGS)
+  rollNumber = signal(1234);
+  studentDetails = signal({ name: 'Tris', city: 'Lagos' });
+  cityList = signal(['Lag', ' Ph', ' Abj']);
+
+  // HOW TO GIVE A SIGNAL A DATATYPE
+  courseDuration: Signal<string> = signal('Three Months');
+
+  // CREATING A FXN TO CHANGE A SIGNAL
+  onChangeCourseName() {
+    this.courseName.set("Java")
+  };
+
+  constructor() {
+    console.log('Before' + this.courseName());
+
+    setTimeout(() => {
+      // TO CHANGE A 'SIGNAL' YOU USE THE SET() METHOD
+      this.courseName.set('VUE JS');
+      console.log('After' + this.courseName());
+    }, 3000);
+  }
 }
