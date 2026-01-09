2151849954fe2a7ea816907758ff8e4a58b77f08
Beattris <beatriceebirim@gmail.com>
2026-01-09 04:53:20 +0100

    modified the src folder

--- src/app/Components/data-binding/data-binding.ts
+++ src/app/Components/data-binding/data-binding.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-data-binding',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './data-binding.html',
   styleUrl: './data-binding.css',
 })
@@ -10,8 +11,24 @@ export class DataBinding {
   // DATA BINDING IS SIMPLY HOW WE BOUND THE DATA IN A TS
   // CLASS TO UNTO THE UI.
 
-  courseName = "Angular 21 walk through"
+  courseName = 'Angular 21 walk through';
 
   // STORING AND USING CLASSNAMES FROM THE TS FILE
-  className = "primary"
+  className = 'primary';
+
+  inputType = 'date';
+
+  // FUNCTIONS CREATION
+  showMessage() {
+    alert('Welcome User');
+  }
+
+  onStateChanged() {
+    alert('Option changed');
+  }
+
+  // CREATING FXNS VALUES CAN BE PASSED TO
+  onChangeCourseName(text:string) {
+    this.courseName = text;
+  };
 }
