3e62f23042f8d6af38d64104099cd55b16342606
Beattris <beatriceebirim@gmail.com>
2026-01-10 04:46:40 +0100

    modified the dynamic-css component to use ngStyle

--- src/app/Components/dynamic-css-comp/dynamic-css-comp.ts
+++ src/app/Components/dynamic-css-comp/dynamic-css-comp.ts
@@ -1,17 +1,35 @@
-import { NgClass } from '@angular/common';
-import { Component } from '@angular/core';
+import { NgClass, NgStyle } from '@angular/common';
+import { Component, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-dynamic-css-comp',
-  imports: [NgClass, FormsModule],
+  imports: [NgClass, FormsModule, NgStyle],
   templateUrl: './dynamic-css-comp.html',
   styleUrl: './dynamic-css-comp.css',
 })
 export class DynamicCssComp {
-  myClassName: string = "bg-warning";
+  myClassName: string = 'bg-warning';
 
-  isActive: boolean = false;  
+  isActive: boolean = false;
 
   productPrice: number = 500;
+
+  isSidePanelOpen = signal(false);
+
+  // USING NG STYLE
+  divBackColor: string = '';
+
+  myCss = {
+    color: 'white',
+    backgroundColor: 'black',
+    fontSize: '35px',
+  };
+
+  openSidePanel() {
+    this.isSidePanelOpen.set(true)
+  };
+  closeSidePanel() {
+    this.isSidePanelOpen.set(false)
+  };
 }
