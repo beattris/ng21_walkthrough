618741be7cb7e96b9d740251c90d4de6ddfcdeaf
Beattris <beatriceebirim@gmail.com>
2026-01-10 04:14:23 +0100

    modified the dynamic-css component with ngClass use-cases

--- src/app/Components/dynamic-css-comp/dynamic-css-comp.ts
+++ src/app/Components/dynamic-css-comp/dynamic-css-comp.ts
@@ -1,11 +1,17 @@
+import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-dynamic-css-comp',
-  imports: [],
+  imports: [NgClass, FormsModule],
   templateUrl: './dynamic-css-comp.html',
   styleUrl: './dynamic-css-comp.css',
 })
 export class DynamicCssComp {
+  myClassName: string = "bg-warning";
 
+  isActive: boolean = false;  
+
+  productPrice: number = 500;
 }
