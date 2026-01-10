229547717ad88c50be0ca1fb0d3e0d761240af7f
Beattris <beatriceebirim@gmail.com>
2026-01-10 03:43:08 +0100

    modified the controlFlow component, more use-cases covered

--- src/app/Components/control-flow-comp/control-flow-comp.ts
+++ src/app/Components/control-flow-comp/control-flow-comp.ts
@@ -1,9 +1,10 @@
 import { Component, signal, Signal, WritableSignal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-control-flow-comp',
-  imports: [FormsModule],
+  imports: [FormsModule, RouterLink],
   templateUrl: './control-flow-comp.html',
   styleUrl: './control-flow-comp.css',
 })
@@ -16,7 +17,34 @@ export class ControlFlowComp {
 
   toggleDivVisibility() {
     this.isSuccessDivVisible.set(!this.isSuccessDivVisible());
-  };
+  }
 
-  // 
+  // USE CASE FOR USING @FOR
+  offerList: string[] = [
+    '20% off for Opay users',
+    '10% off for Moniepoint users',
+    '15% off for Prepaid users',
+  ];
+
+  productCategoryList: string[] = [
+    'Cameras',
+    'Laptops',
+    'Mobile Phones',
+    'Computers',
+    'Headphones',
+    'Mouse',
+    'Keyboards',
+  ];
+
+  // SHOWING SPECIFIC TYPE OF DATA
+  employeeList = [
+    {name: "AAA", city: "jdfhjd", isActive: false},
+    {name: "BBB", city: "fgfg", isActive: false},
+    {name: "CCC", city: "efrr", isActive: true},
+    {name: "DDD", city: "hetet", isActive: true},
+    {name: "FFF", city: "strtrw", isActive: false},
+    {name: "GGG", city: "kyiky", isActive: false},
+    {name: "HHH", city: "itujhn", isActive: true},
+    {name: "JJJ", city: "vdjusr", isActive: false},
+  ]
 }
